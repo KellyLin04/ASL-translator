fb1474cb7a388e47f009c2cf8c44fb5e6fd6f54f
Kelly Lin <kellylin@Kellys-MacBook-Pro.local>
2021-02-20 23:29:43 -0800

    text on screen

--- MediaPipe_trained.py
+++ MediaPipe_trained.py
@@ -18,6 +18,7 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture(0)
 collection = []
+text2 = ""
 while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -51,27 +52,47 @@ while cap.isOpened():
         
         output_test2 = np.transpose(output3)
         res2 = np.round(np.matmul(np.transpose(W),output_test2))    
-
+        
+        text = ""
+        classification = res2
+        if (classification == 1):
+            text = "Hello"
+        elif (classification == 2):
+            text = "i"
+        elif (classification == 3):
+            text = "m"
+        elif (classification == 4):
+            text = "a"
+        elif (classification == 5):
+            text = "y"
             
-        #output classification
-        collection.append(res2)
+        # font 
+        font = cv2.FONT_HERSHEY_SIMPLEX 
+        # org 
+        org = (50, 50) 
+        org2 = (50, 150)
+        # fontScale 
+        fontScale = 1
+        # Blue color in BGR 
+        color = (255, 0, 0) 
+        # Line thickness of 2 px 
+        thickness = 2
         
-        if (len(collection) >= 30):
-            classification = np.round(np.mean(collection))
-            if (classification == 1):
-                print("Hello")
-            elif (classification == 2):
-                print("I")
-            elif (classification == 3):
-                print("m")
-            elif (classification == 4):
-                print("a")
-            elif (classification == 5):
-                print("y")
-            collection = []
-
-        
+        k = cv2.waitKey(5)
     
+        image = cv2.putText(image,text, org, font,  
+                   fontScale, color, thickness, cv2.LINE_AA)  
+        
+        if (k%256 == 32):
+            text2 += text
+        elif (k%256 == ord("e")):
+            text2 += "_"
+        elif (k%256 == ord("r")):
+            text2 = ""
+            
+        image = cv2.putText(image,text2, org2, font,  
+               2*fontScale, (0,0,0), thickness, cv2.LINE_AA) 
+             
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
         break
